15136f6def4cac074dcc878bba1a0381d1570f6a
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-26 21:42:07 -0400

    Terminando parte funcional de calculos.

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 let tasaPEN =Number(15.85);
 let tasaUsd =Number(63.40);
 
+//Para recibir
 document.querySelector("#precio").addEventListener("keyup", leer);
 document.querySelector("#tasaPen").value=tasaPEN;
 document.querySelector("#tasaUsd").value=tasaUsd;
 
+//Para enviar
+document.querySelector("#precioEnviar").addEventListener("keyup", leerEnviar);
+
+//Para enviar
+function leerEnviar() {
+    (document.querySelector("#precioEnviar").value)
+    ? calcularEnviar()
+    : vaciarEnviar();
+}
+function vaciarEnviar() {
+    document.querySelector("#bsEquivalentesEnviar").value="";
+    document.querySelector("#totalEnviar").value="";
+}
+function calcularEnviar() {
+    let cantidadEnviar = Number(document.querySelector("#precioEnviar").value);
+    let bsEquivalentesEnviar = Number((cantidadEnviar*tasaPEN));
+    let totalEnviar = Number(((cantidadEnviar*tasaPEN)/(tasaUsd)).toFixed(2));
+    document.querySelector("#totalEnviar").value=totalEnviar;
+    document.querySelector("#bsEquivalentesEnviar").value=bsEquivalentesEnviar;
+}
 
 
+//Para recibir
 function leer() {
     (document.querySelector("#precio").value)
     ? calcular()
     : vaciar();
 }
-
 function vaciar() {
     document.querySelector("#bsEquivalentes").value="";
     document.querySelector("#total").value="";
 }
-
 function calcular() {
     let cantidad = Number(document.querySelector("#precio").value);
     let bsEquivalentes = Number(cantidad*tasaUsd);
